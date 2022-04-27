f989f32089d6d3748f1f3a38274688bd8fb3a041
noii <macbookpro@noii.local>
2022-04-27 13:48:06 +0700

    idk

--- collections/config.js
+++ collections/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const provider = require("../provider/db");
 require('dotenv').config();
-mongoose.connect(process.env.BASE_URL + process.env.DB, { useUnifiedTopology: true });
+mongoose.connect(process.env.BASE_URL + process.env.DB, { useNewUrlParser: true});
 // mongoose.disconnect();
 module.exports = mongoose;
